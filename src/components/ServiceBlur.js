c5b1e66fe830385a0d22375d9ab9d78e5d9d5611
Sjur Espedal <sjurbes@gmail.com>
2021-04-22 10:54:05 +0100

    create UI for blur image

--- src/components/ServiceBlur.js
+++ src/components/ServiceBlur.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import {Button, Grid, makeStyles, Typography} from "@material-ui/core";
+import {red} from "@material-ui/core/colors";
 
+const useStyles = makeStyles(() => ({
+	button: {
+	}
+}))
 
 export default function ImageBlur(props) {
-	return <div>Image blur</div>
+	const styles = useStyles()
+	return (
+		<Grid container direction={"row"} >
+			<Grid item xs>
+				<Typography variant={"h1"}>Image blur</Typography>
+
+				<Button variant="contained" color="primary" onClick={() => console.log("Blur")}> Blur image</Button>
+			</Grid>
+
+		</Grid>
+	)
 
 }
